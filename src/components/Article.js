84ab062131077188ca97ca69c004bcad6377d432
Joey Kishiuchi <josephkishiuchi@outlook.com>
2020-08-31 20:05:01 -0700

    Styled article. Added non-funcitoning nominate button

--- src/components/Article.js
+++ src/components/Article.js
@@ -1,12 +1,16 @@
 import React from 'react';
-// import './Article.scss';
+import './Article.scss';
+import {Button} from '@material-ui/core';
 
 function Article({movie}) {
   return(
     <div className="article-container">
       <img className="article-poster" alt="movie-poster" src={movie.Poster}></img>
-      <span className="article-title">{movie.Title}</span>
-      <span className="article-year">{movie.Year}</span>
+      <div className="article-description">
+        <span className="article-title">{movie.Title}</span>
+        <span className="article-year">{movie.Year}</span>
+      </div>
+      <Button variant="contained" color="primary">Nominate</Button>
     </div>
   );
 };
