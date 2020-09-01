acf141f2e7e2fb9e2b26a6430a9b1d21dcd51cc0
Joey Kishiuchi <josephkishiuchi@outlook.com>
2020-08-31 18:58:28 -0700

    Added axios request to OMDb api. Set api key in env file

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,26 @@
-import React from 'react';
-import './App.css';
+import React, {useEffect} from 'react';
+import './App.scss';
 import SearchBar from './SearchBar.js';
+import axios from 'axios';
 
 function App() {
+
+  useEffect(() => {
+    axios({
+      method: 'GET',
+      url: `http://www.omdbapi.com/?apikey=${process.env.REACT_APP_OMDB_API_KEY}`,
+      params: {
+        s: "mission impossible"
+      }
+    })
+    .then(res => {
+      console.log(res.data.Search);
+    })
+  },[]);
+
   return (
     <div className="App">
-      <h1>The Shoppies</h1>
+      <h1 classname="main-header">The Shoppies</h1>
       <SearchBar />
     </div>
   );
