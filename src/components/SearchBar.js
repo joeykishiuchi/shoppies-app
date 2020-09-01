390060f7c73cd9fde77dba5bd54cdc53c6632a8d
Joey Kishiuchi <josephkishiuchi@outlook.com>
2020-08-31 18:32:07 -0700

    Styled Searchbar

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,5 +1,6 @@
 import React, {useState, useEffect} from 'react';
 import {TextField} from '@material-ui/core'
+import './SearchBar.scss';
 
 function SearchBar() {
   const [text, setText] = useState('');
@@ -11,7 +12,12 @@ function SearchBar() {
         onSubmit={event => event.preventDefault()}
         autoComplete="off"
       >
-        <TextField />
+        <TextField 
+          placeholder="Search Movies"
+          variant="outlined"
+          value={text}
+          onChange={event => setText(event.target.value)}
+        />
       </form>
     </div>
   );
